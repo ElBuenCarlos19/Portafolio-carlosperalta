9154ed725792e8858fe2f88560eb3aa06b80ba44
Carlos Peralta <carlosperalta19102004@gmail.com>
2025-06-11 15:34:11 -0500

    Se agrego analiticas

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type React from "react"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
+import { Analytics } from '@vercel/analytics/next';
+
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -22,7 +24,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-dark text-white`}>{children}</body>
+      <body className={`${inter.className} bg-dark text-white`}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   )
 }
